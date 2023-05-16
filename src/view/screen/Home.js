d95271da87a62f2b7ce58b8277a729fadb5f3ef9
JAYESH-7 <jaymittal2002@gmail.com>
2023-05-16 16:36:21 +0530

    commit

--- src/view/screen/Home.js
+++ src/view/screen/Home.js
@@ -16,7 +16,7 @@ export default function Home(){
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>First</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
